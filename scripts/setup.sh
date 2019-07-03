76831d1090c10250cadd7749c306e27f7fdbaba3
carlov93 <36641441+carlov93@users.noreply.github.com>
2019-07-03 13:32:25 +0200

    ddd

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # create virtual environment
-python -m venv venvMasterarbeit
+pip3 install virtualenv
+virtualenv venvMasterarbeit
 source venvMasterarbeit/bin/activate
 pip install ipykernel
 ipython kernel install --user --name=masterarbeit
 
 # install packages
-pip install torch
-pip install pandas
-pip install tensorflow
-pip install keras
-pip install sklearn
-pip install matplotlib
+pip3 install torch
+pip3 install pandas
+pip3 install tensorflow
+pip3 install keras
+pip3 install sklearn
+pip3 install matplotlib
