578803361643bd865884bece11116eb21004ca61
carlov93 <36641441+carlov93@users.noreply.github.com>
2019-08-28 16:18:25 +0200

    minor changes and new artifical data

--- src/ai/utils/loss_module.py
+++ src/ai/utils/loss_module.py
@@ -45,29 +45,4 @@ class LossMle(torch.nn.Module):
         term = torch.pow((target_data - y_hat) / torch.exp(tau), 2) + 2 * tau
         loss_batches = torch.sum(input=term, dim=1) / self.input_size
         mean_loss = torch.sum(loss_batches)/self.batch_size    
-        list_loss.append(mean_loss.item())
         return mean_loss
-
-class LossMseInference(torch.nn.Module):
-    """
-    This class is for prediction purpose only. Returns loss for each sample of a batch
-    """
-    def __init__(self, input_size, batch_size):
-        super(LossMseInference, self).__init__()
-        self.input_size = input_size
-        self.batch_size = batch_size
-
-    def forward(self, output, target_data):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. 
-        """
-        # Compute loss
-        loss_batches = torch.sum((output - target_data)**2, dim=1) / self.input_size
-        return loss_batches
-
-    def share_per_sensor(self, output, target_data):
-        loss_per_sensor = (output - target_data)**2
-        sum_loss = torch.sum((output - target_data)**2, dim=1)
-        result = loss_per_sensor/sum_loss[:,None]
-        return result
--- src/ai/utils/predictor.py
+++ src/ai/utils/predictor.py
@@ -6,9 +6,8 @@ import numpy as np
 import builtins
 
 class PredictorMse():
-    def __init__(self, model, criterion, path_data, columns_to_ignore, threshold_anomaly):
+    def __init__(self, model, path_data, columns_to_ignore, threshold_anomaly):
         self.model = model
-        self.criterion = criterion
         self.path_data = path_data
         self.column_names_data = self.get_column_names_data()
         self.columns_to_ignore = columns_to_ignore
@@ -24,16 +23,15 @@ class PredictorMse():
                                if column_name not in self.columns_to_ignore+["ID"]]
         column_names_predicted = [column_name+" predicted" for column_name in self.column_names_data 
                                   if column_name not in self.columns_to_ignore+["ID"]]
-        column_names_loss_per_sensor = [column_name+" share of loss " for column_name in self.column_names_data 
+        column_names_loss_per_sensor = [column_name+" share of reconstruction error " for column_name in self.column_names_data 
                                         if column_name not in self.columns_to_ignore+["ID"]]
         column_names_residuals= ["residual "+column_name for column_name in self.column_names_data 
                                  if column_name not in self.columns_to_ignore+["ID"]]
         
-        column_names = ["ID"] + column_names_target + column_names_predicted + ["loss"] + column_names_loss_per_sensor + column_names_residuals
+        column_names = ["ID"] + column_names_target + column_names_predicted + ["reconstruction error"] + column_names_loss_per_sensor + column_names_residuals
         return column_names
           
     def predict(self, input_data, target_data):
-        results = pd.DataFrame(columns=self.create_column_names_result())
         self.model.eval()
         with torch.no_grad():
             # Store ID of target sample 
@@ -48,10 +46,6 @@ class PredictorMse():
 
             # Forward propagation
             output = self.model(input_data, hidden)
-
-            # Calculate loss
-            loss = self.criterion(output, target_data)
-            loss_share_per_sensor = self.criterion.share_per_sensor(output, target_data)
             
             batch_results= []
             for batch in range(self.model.batch_size):
@@ -59,11 +53,12 @@ class PredictorMse():
                 predicted_data = output[batch,:].data.numpy().tolist()
                 ground_truth = target_data[batch,:].data.numpy().tolist()
                 residuals = [target_i - prediction_i for target_i, prediction_i in zip(ground_truth, predicted_data)]
-                loss_share_per_sensor_np = loss_share_per_sensor[batch,:].data.numpy().tolist()
+                reconstruction_error = sum([(ground_truth_i - predicted_i)**2 for ground_truth_i, predicted_i in zip(ground_truth, predicted_data )]) / self.model.input_dim
+                reconstruction_error_per_sensor = [(ground_truth_i - predicted_i)**2/reconstruction_error for ground_truth_i, predicted_i in zip(ground_truth, predicted_data )]
 
                 # Add values to dataframe
-                data = [id_target[batch].item()] + ground_truth + predicted_data + [loss[batch].item()] + \
-                loss_share_per_sensor_np + residuals
+                data = [id_target[batch].item()] + ground_truth + predicted_data + [reconstruction_error] + \
+                reconstruction_error_per_sensor + residuals
                 batch_results.append(data)
                         
         return batch_results
@@ -87,9 +82,8 @@ class PredictorMse():
         return results_prediction
     
 class PredictorMultiTaskLearning():
-    def __init__(self, model, criterion, path_data, columns_to_ignore, threshold_anomaly):
+    def __init__(self, model, path_data, columns_to_ignore, threshold_anomaly):
         self.model = model
-        self.criterion = criterion
         self.path_data = path_data
         self.column_names_data = self.get_column_names_data()
         self.columns_to_ignore = columns_to_ignore
@@ -105,18 +99,17 @@ class PredictorMultiTaskLearning():
                                if column_name not in self.columns_to_ignore+["ID"]]
         column_names_predicted = [column_name+" predicted" for column_name in self.column_names_data 
                                   if column_name not in self.columns_to_ignore+["ID"]]
-        column_names_loss_per_sensor = [column_name+" share of loss " for column_name in self.column_names_data 
+        column_names_reconstruction_error_per_sensor = [column_name+" share of reconstruction error " for column_name in self.column_names_data 
                                         if column_name not in self.columns_to_ignore+["ID"]]
         column_names_residuals= ["residual "+column_name for column_name in self.column_names_data 
                                  if column_name not in self.columns_to_ignore+["ID"]]
         column_names_latent_space= ["latent_space_"+str(i) for i in range(self.model.n_hidden_fc_ls_analysis)]
         
-        column_names = ["ID"] + column_names_target + column_names_predicted + ["loss"] + \
-                       column_names_loss_per_sensor + column_names_residuals + column_names_latent_space
+        column_names = ["ID"] + column_names_target + column_names_predicted + ["reconstruction error"] + \
+                       column_names_reconstruction_error_per_sensor + column_names_residuals + column_names_latent_space
         return column_names
     
     def predict(self, input_data, target_data):
-        results = pd.DataFrame(columns=self.create_column_names_result())
         self.model.eval()
         with torch.no_grad():
             # Store ID of target sample 
@@ -132,10 +125,6 @@ class PredictorMultiTaskLearning():
             # Forward propagation
             prediction, _ = self.model(input_data, hidden)
             latent_space = self.model.current_latent_space
-
-            # Calculate loss (subnetwork for latent space analysis not longer considered)
-            loss_prediction_network = self.criterion(prediction, target_data) 
-            loss_share_per_sensor = self.criterion.share_per_sensor(prediction, target_data)
             
             batch_results= []
             for batch in range(self.model.batch_size):
@@ -144,11 +133,13 @@ class PredictorMultiTaskLearning():
                 ground_truth = target_data[batch,:].data.numpy().tolist()
                 residuals = [target_i - prediction_i for target_i, prediction_i in zip(ground_truth, predicted_data)]
                 latent_space_np = latent_space[batch,:].data.numpy().tolist()
+                reconstruction_error = sum([(ground_truth_i - predicted_i)**2 for ground_truth_i, predicted_i in zip(ground_truth, predicted_data )]) / self.model.input_dim
+                reconstruction_error_per_sensor = [(ground_truth_i - predicted_i)**2/reconstruction_error for ground_truth_i, predicted_i in zip(ground_truth, predicted_data )]
                 loss = loss_prediction_network[batch].item()
                 loss_share_per_sensor_np = loss_share_per_sensor[batch,:].data.numpy().tolist()
                     
                 # Add values to dataframe
-                data = [id_target[batch].item()] + ground_truth + predicted_data + [loss] + loss_share_per_sensor_np + \
+                data = [id_target[batch].item()] + ground_truth + predicted_data + [reconstruction_error] + reconstruction_error_per_sensor + \
                 residuals + latent_space_np
                 batch_results.append(data)
                         
@@ -174,59 +165,58 @@ class PredictorMultiTaskLearning():
         return results_prediction
     
 class PredictorMle():
-    def __init__(self, model, criterion, path_data, columns_to_ignore):
+    def __init__(self, model, path_data, columns_to_ignore, threshold_anomaly, no_standard_deviation):
         self.model = model
-        self.criterion = criterion
         self.path_data = path_data
         self.column_names_data = self.get_column_names_data()
         self.columns_to_ignore = columns_to_ignore
+        self.threshold_anomaly = threshold_anomaly
+        self.no_standard_deviation = no_standard_deviation
 
     def get_column_names_data(self):
         with open(self.path_data, 'r') as f:
             header = f.readline().replace('\n','')
             return header.split(",")
         
-    def create_column_names_result_mse(self):
+    def create_column_names_result(self):
         column_names_target = [column_name+" target" for column_name in self.column_names_data if column_name not in self.columns_to_ignore+["ID"]]
         column_names_mu_predicted = [column_name+" mu predicted" for column_name in self.column_names_data if column_name not in self.columns_to_ignore+["ID"]]
         column_names_sigma_predicted = [column_name+" sigma predicted" for column_name in self.column_names_data if column_name not in self.columns_to_ignore+["ID"]]
         column_names_normalised_residuals= [column_name+" normalised residual" for column_name in self.column_names_data if column_name not in self.columns_to_ignore+["ID"]]
         return ["ID"] + column_names_target + column_names_mu_predicted + column_names_sigma_predicted + ["normalised residual"] +  column_names_normalised_residuals
            
-    def predict(self, data_loader):
+    def predict(self, input_data, target_data):
         self.model.eval()
         with torch.no_grad():
-                input_data, target_data = data
+            # Store ID of target sample 
+            id_target = target_data[:,0]   #ID must be on first position!
                 
-                # Store ID of target sample 
-                id_target = int(target_data[:,0].item())   #ID must be on first position!
-                
-                # De-select ID feature in both input_data and target data for inference
-                input_data = torch.from_numpy(input_data.numpy()[:,:,1:])   # ID must be on first position!
-                target_data = torch.from_numpy(target_data.numpy()[:,1:])   # ID must be on first position!
+            # De-select ID feature in both input_data and target data for inference
+            input_data = torch.from_numpy(input_data.numpy()[:,:,1:])   # ID must be on first position!
+            target_data = torch.from_numpy(target_data.numpy()[:,1:])   # ID must be on first position!
 
-                # Initilize Hidden and Cell State
-                hidden = self.model.init_hidden()
+            # Initilize Hidden and Cell State
+            hidden = self.model.init_hidden()
 
-                # Forward propagation
-                mu, tau = self.model(input_data, hidden)
+            # Forward propagation
+            mu, tau = self.model(input_data, hidden)
                 
-                # Because of the transformation of sigma inside the LossModuleMle (σ_t = exp(τ_t))
-                # we have to revert this transformation with exp(tau_i).
-                sigma_batches = torch.exp(tau)
+            # Because of the transformation of sigma inside the LossModuleMle (σ_t = exp(τ_t))
+            # we have to revert this transformation with exp(tau_i).
+            sigma_batches = torch.exp(tau)
                 
-                # Reshape and Calculate prediction metrics
-                batch_results= []
-                for batch in range(self.model.batch_size):
-                    mu_predicted = mu[batch,:].data.numpy().tolist()
-                    ground_truth = target_data[batch,:].data.numpy().tolist()
-                    sigma_predicted = sigma_batches[batch,:].data.numpy().tolist()
-                    normalised_residual_per_sensor = [(target_i - prediction_i)/sigma_i for target_i, prediction_i, sigma_i in zip(ground_truth, mu_predicted, sigma)]
-                    normalised_residual = sum(normalised_residual_per_sensor)
+            # Reshape and Calculate prediction metrics
+            batch_results= []
+            for batch in range(self.model.batch_size):
+                mu_predicted = mu[batch,:].data.numpy().tolist()
+                ground_truth = target_data[batch,:].data.numpy().tolist()
+                sigma_predicted = sigma_batches[batch,:].data.numpy().tolist()
+                normalised_residual_per_sensor = [(target_i - prediction_i) / sigma_i for target_i, prediction_i, sigma_i in zip(ground_truth, mu_predicted, sigma_predicted)]
+                normalised_residual = sum(normalised_residual_per_sensor) / self.model.input_dim
                     
-                    data = [id_target[batch].item()] + ground_truth + mu_predicted + sigma_predicted + [normalised_residual] + \
-                    normalised_residual_per_sensor
-                    batch_results.append(data)
+                data = [id_target[batch].item()] + ground_truth + mu_predicted + sigma_predicted + [normalised_residual] + \
+                normalised_residual_per_sensor
+                batch_results.append(data)
 
         return batch_results
 
@@ -234,17 +224,20 @@ class PredictorMle():
         # Drop all empty columns
         results_prediction.drop(results_prediction.columns[results_prediction.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
         
-        # smooth loss to 
-        smoothed_normalised_residual = []
-        for i,value in enumerate(results_prediction.loc[:,"normalised residual"]):
-            if i==0:
-                smoothed_normalised_residual.append(value)
-            else:
-                x = smooth_rate  * value + (1 - smooth_rate) * smoothed_loss[-1]
-                smoothed_normalised_residual.append(x)
-        results_prediction["smoothed_normalised_residual"]=smoothed_normalised_residual
-        
-        # tag sample as an anomaly (1) if loss is higher than given threshold, otherwhise 0 
-        results_prediction["anomaly"] = np.where(results_prediction["smoothed_normalised_residual"]>=self.threshold_anomaly, 1, 0)
+        for no_sensor in range(1,self.model.input_dim+1):
+            sensor_target = results_prediction.iloc[:,no_sensor].values
+            sensor_mu = results_prediction.iloc[:,no_sensor+self.model.input_dim].values
+            sensor_sigma = results_prediction.iloc[:,no_sensor+2*self.model.input_dim].values
+            
+            # tag sample as an anomaly (1) if sensor value is higher or lower than given confidence band, otherwhise 0 
+            anomaly = []
+            for target_i, mu_i, sigma_i in zip(sensor_target, sensor_mu, sensor_sigma):
+                if target_i < (mu_i - self.no_standard_deviation * sigma_i):
+                    anomaly.append(1)
+                elif target_i > (mu_i + self.no_standard_deviation * sigma_i):
+                    anomaly.append(1)
+                else:
+                    anomaly.append(0)
+            
+            results_prediction["Anomaly Sensor_"+str(no_sensor)]= anomaly
         return results_prediction
-    
