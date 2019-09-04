71096fa3a46715bdeefcc24cc152d754107cfd01
carlov93 <36641441+carlov93@users.noreply.github.com>
2019-09-04 16:32:32 +0200

    wording

--- src/ai/utils/scaler.py
+++ src/ai/utils/scaler.py
@@ -7,13 +7,13 @@ class DataScaler():
         self.scaler = StandardScaler()
         self.features_not_to_scale = features_not_to_scale
         
-    def scale_data_test_dataset(self, test_data, mean_test_data, var_test_data):
+    def scale_test_dataset(self, test_data, mean_train_data, var_train_data):
         data_numpy = test_data.values
         # Transform data for prediction with mean and variance of training data
         test_scaled = np.zeros(shape=(data_numpy.shape[0], data_numpy.shape[1]))
         
         i = 0
-        for mean, var in zip(mean_test_data, var_test_data):
+        for mean, var in zip(mean_train_data, var_train_data):
             test_scaled[:,i] = np.subtract(data_numpy[:,i], mean)
             test_scaled[:,i] = np.divide(test_scaled[:,i], np.sqrt(var))
             i +=1
