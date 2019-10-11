5b2cf1f0135e660163582125d15b2e7be2e1cf95
carlov93 <36641441+carlov93@users.noreply.github.com>
2019-10-11 16:30:29 +0200

    updates in cross validation

--- src/ai/utils/cross_validation.py
+++ src/ai/utils/cross_validation.py
@@ -1,36 +1,30 @@
 import pandas as pd
 
 class CrossValidationProvider():
-    def __init__(self, path, no_folds, amount_data, stake_training_data, ignored_features):
+    def __init__(self, path, no_folds, amount_data, ignored_features):
         self.path = path
-        self.train_data = None
-        self.test_data = None
+        self.data = None
         self.no_folds = no_folds
         self.amount_data = amount_data
-        self.stake_training_data = stake_training_data
         self.ignored_features = ignored_features
         
-    def split_training_test(self):
+    def load_data(self):
         # Read hole dataset und drop features
         dataset = pd.read_csv(self.path)
-        dataset = dataset.drop(labels=self.ignored_features, axis=1)
-        
-        # Select training and test dataset
-        sub_dataset = dataset.iloc[:self.amount_data,:]
-        amount_training_data = round(len(sub_dataset)*self.stake_training_data)
-        self.train_data = sub_dataset.iloc[:amount_training_data,:]
-        self.test_data = sub_dataset.iloc[amount_training_data:,:]
+        data = dataset.drop(labels=self.ignored_features, axis=1)
+        self.data = data.iloc[:self.amount_data,:]
         
     def split_dataset_in_folds(self):
-        amount_each_fold = round(len(self.train_data) / self.no_folds)
+        amount_each_fold = round(len(self.data) / self.no_folds)
         
         folds = []
         for fold_number in range(1,self.no_folds+1):
-            fold = self.train_data.iloc[(fold_number-1)*amount_each_fold:fold_number*amount_each_fold,:]
+            fold = self.data.iloc[(fold_number-1)*amount_each_fold:fold_number*amount_each_fold,:]
             folds.append(fold)
         return folds
         
     def provide_data(self):
-        self.split_training_test()
+        self.load_data()
         folds = self.split_dataset_in_folds()
-        return self.test_data, folds 
+        return folds
+    
--- src/ai/utils/scaler.py
+++ src/ai/utils/scaler.py
@@ -19,13 +19,14 @@ class DataScaler():
             i +=1
         return test_scaled
     
-    def scale_data(self, train_data, validation_data):
+    def scale_data(self, train_data, validation_data, test_data):
         if len(self.features_not_to_scale) == 0:
             # Initialise standard scaler
             self.scaler.fit(train_data)
             # Transform data
             train_scaled = self.scaler.transform(train_data)
             validation_scaled = self.scaler.transform(validation_data)
+            test_scaled = self.scaler.transform(test_data)
         
         else:
             # seperate categorical and continous features
@@ -33,17 +34,22 @@ class DataScaler():
             continous_features_train = train_data.drop(labels=self.features_not_to_scale, axis=1)
             categorical_features_validation = validation_data.loc[:, self.features_not_to_scale]
             continous_features_validation = validation_data.drop(labels=self.features_not_to_scale, axis=1)
+            categorical_features_test = test_data.loc[:, self.features_not_to_scale]
+            continous_features_test = test_data.drop(labels=self.features_not_to_scale, axis=1)
 
             # Initialise standard scaler
             self.scaler.fit(continous_features_train)
             # Transform data
             continous_train_scaled = self.scaler.transform(continous_features_train)
             continous_validation_scaled = self.scaler.transform(continous_features_validation)
+            continous_test_scaled = self.scaler.transform(continous_features_test)
 
             # Combine categorical and scaled features 
             train_scaled = np.concatenate((continous_train_scaled, categorical_features_train), axis=1)
             validation_scaled = np.concatenate((continous_validation_scaled, categorical_features_validation), axis=1)
-        return train_scaled, validation_scaled
+            test_scaled = np.concatenate((continous_test_scaled, categorical_features_test), axis=1)
+        return train_scaled, validation_scaled, test_scaled
     
     def provide_statistics(self):
         return self.scaler.mean_, self.scaler.var_
+    
